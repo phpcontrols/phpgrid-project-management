a31344baaaac924c447a73e5c7f8229212f0bcae
phpcontrols <richard@phpcontrols.com>
2017-03-16 18:15:34 +0800

    Index page

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Simple Project Management | phpGrid</title>
+</head>
+<body>
+
+<style>
+.centered {
+    margin: 0;
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align:center;
+}
+.footer {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  padding: 1rem;
+  background-color: #efefef;
+  text-align: center;
+}
+</style>
+
+<div class="centered">
+<h1>My Simple Project Management</h1>
+
+<a href="manager/clients.php">Login as manager</a> | <a href="employee/tasks.php">Login as employee</a>
+</div>
+
+<div class="footer"><strong>Build-From-Scratch Series</strong> | phpGrid &copy; <?php echo date('Y'); ?>.</div>
+
+</body>
+</html>
